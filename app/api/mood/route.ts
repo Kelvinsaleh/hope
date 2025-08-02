06e8d920eb6153a49a451cf616867bf963faee41
Hitman <Knsalee@gmail.com>
2025-08-02 13:02:34 +0300

    Update route.ts

--- app/api/mood/route.ts
+++ app/api/mood/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
   const API_URL =
     process.env.BACKEND_API_URL ||
-    "https://ai-therapist-agent-backend.onrender.com";
+    "https://hope-backend-ai.onrender.com";
   const token = req.headers.get("Authorization");
 
   if (!token) {
